1e53b4d3eeb1192c32d1b7d0bead551bf80406e0
sandz <30sandz@gmail.com>
2026-07-11 00:08:47 +0530

    Ex:4

--- DeepSkilling/Mandatory/Week_3/Spring_rest_Ex_4/demo/src/main/java/com/example/demo/Controller.java
+++ DeepSkilling/Mandatory/Week_3/Spring_rest_Ex_4/demo/src/main/java/com/example/demo/Controller.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +27,19 @@ public class Controller{
 
     }
 
+    @GetMapping("/country/{code}")
+    public Country getCountryById(@PathVariable String code){
+        log.info("Method Find by ID entered");
+
+        Map<String, Country> list = context.getBeansOfType(Country.class);
+
+        for(Map.Entry<String, Country> c: list.entrySet()){
+            if(c.getValue().getCode().equalsIgnoreCase(code)){
+                return c.getValue();
+            }
+        }
+
+        return null;
+        
+    }
 }
