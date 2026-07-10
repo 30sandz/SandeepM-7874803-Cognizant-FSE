2e9d84d85ca414fdb707e531785bad4e1d289f18
sandz <30sandz@gmail.com>
2026-07-10 15:00:49 +0530

    Exercise 4: REST response

---
+++ DeepSkilling/Mandatory/Week_3/Spring_rest_Ex_4/demo/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller{
+
+    private static final Logger log = LoggerFactory.getLogger(Controller.class);
+    ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+
+    @GetMapping("/country")
+    public Country getCountryIndia(){
+        log.info("Method entered");
+
+        return context.getBean("india",Country.class);
+
+    }
+
+}
